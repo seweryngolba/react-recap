3ab86ce9bd096cd06303fb148b6779f71e4c1f7c
Seweryn Golba <sgoolb@gmail.com>
2023-07-20 23:26:06 +0200

    next lesson

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 import Welcome from "./components/Welcome";
 import Clock from "./components/Clock";
 import { useState } from "react";
+import LoginControl from "./components/LoginControl";
+import Blog from "./components/Blog";
 
 function App() {
   const [flag, setFlag] = useState(true);
@@ -11,18 +13,38 @@ function App() {
     lastname: "Diaz",
   };
 
-  const toggle = () => {
-    console.log("Toggle Clicked");
-    setFlag(!flag);
-  };
+  const posts = [
+    {
+      id: 1,
+      title: "Hello World",
+      content: "Welcome in React",
+    },
+    {
+      id: 2,
+      title: "Hello People",
+      content: "Welcome in JS",
+    },
+    {
+      id: 3,
+      title: "Hello Animals",
+      content: "Welcome in HTML",
+    },
+  ];
+
+  // const toggle = () => {
+  //   console.log("Toggle Clicked");
+  //   setFlag(!flag);
+  // };
 
   return (
     <>
-      <button onClick={() => setFlag(!flag)}>Toggle Clock Component</button>
-      {flag ? <Clock /> : "No Clock Component"}
+      {/* <button onClick={() => setFlag(!flag)}>Toggle Clock Component</button>
+      {flag ? <Clock /> : "No Clock Component"} */}
       {/* <Welcome user="Pedro" />
       <Welcome user="Santiago" />
       <Welcome user="Rodrigo" /> */}
+      {/* <LoginControl /> */}
+      <Blog posts={posts} />
     </>
   );
 }
---
+++ src/components/LoginControl.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+function LoginControl() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const handleLoginClick = () => {
+    setIsLoggedIn(true);
+  };
+  const handleLogoutClick = () => {
+    setIsLoggedIn(false);
+  };
+
+  let button;
+  if (isLoggedIn) {
+    button = <LogoutButton onClick={handleLogoutClick} />;
+  } else {
+    button = <LoginButton onClick={handleLoginClick} />;
+  }
+
+  return <div>{button}</div>;
+}
+
+function LoginButton(props) {
+  return <button onClick={props.onClick}>Login</button>;
+}
+
+function LogoutButton(props) {
+  return <button onClick={props.onClick}>Logout</button>;
+}
+export default LoginControl;
