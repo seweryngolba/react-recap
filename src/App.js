b85de646e0f47ec5fb189363ceecbfc95a8d6dc6
Seweryn Golba <sgoolb@gmail.com>
2023-07-20 20:11:39 +0200

    first lesson

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Welcome from "./components/Welcome";
+import Clock from "./components/Clock";
+import { useState } from "react";
 
 function App() {
+  const [flag, setFlag] = useState(true);
+  const userInfo = {
+    firstname: "Pedro",
+    lastname: "Diaz",
+  };
+
+  const toggle = () => {
+    console.log("Toggle Clicked");
+    setFlag(!flag);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={() => setFlag(!flag)}>Toggle Clock Component</button>
+      {flag ? <Clock /> : "No Clock Component"}
+      {/* <Welcome user="Pedro" />
+      <Welcome user="Santiago" />
+      <Welcome user="Rodrigo" /> */}
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-//import App from "./App";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>Hello, World</h1>);
+
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
